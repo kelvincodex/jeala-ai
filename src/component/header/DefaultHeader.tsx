fef5654b2827aaf69452f6855122a43365121bea
Thankgod Morah <thankgod.morah@qucoon.com>
2024-06-28 07:10:26 +0100

    100% mobile

--- src/component/header/DefaultHeader.tsx
+++ src/component/header/DefaultHeader.tsx
@@ -7,7 +7,7 @@ import {animate, stagger} from "framer-motion";
 
 export const DefaultHeader = ()=>{
     const [isClose, setIsClose] = useState<boolean>(false)
-    const staggerMenuItems = stagger(0.1, { startDelay: 0.15 });
+    const staggerMenuItems = stagger(0.1, { startDelay: 0.10 });
 
     const hoverClassName = 'hover:border-b hover:text-primary-100 hover:border-b-primary-100  transition-all duration-300'
 
@@ -15,15 +15,15 @@ export const DefaultHeader = ()=>{
     useEffect(() => {
         animate(
             "li",
-            !isClose
+            isClose
                 ? { opacity: 1, scale: 1, filter: "blur(0px)" }
                 : { opacity: 0, scale: 0.3, filter: "blur(20px)" },
             {
                 duration: 0.2,
-                delay: !isClose ? staggerMenuItems : 0,
+                delay: isClose ? staggerMenuItems : 0,
             }
         );
-    }, []);
+    }, [isClose, staggerMenuItems]);
     return (
         <nav className={'border-b sticky top-0 bg-white z-50 w-full transition-all duration-300 ease-in-out h-[80px] md:h-[100px] '}>
             <div className={'flex justify-between h-full items-center container-responsive relative'}>
