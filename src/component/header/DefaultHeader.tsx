431ac4de6bb39b8d60fad83bbbd441ed13e71b15
Thankgod Morah <thankgod.morah@qucoon.com>
2024-06-28 09:12:27 +0100

    100% mobile

--- src/component/header/DefaultHeader.tsx
+++ src/component/header/DefaultHeader.tsx
@@ -19,7 +19,7 @@ export const DefaultHeader = ()=>{
                 ? { opacity: 1, scale: 1, filter: "blur(0px)" }
                 : { opacity: 0, scale: 0.3, filter: "blur(20px)" },
             {
-                duration: 0.2,
+                duration: 0,
                 delay: isClose ? staggerMenuItems : 0,
             }
         );
