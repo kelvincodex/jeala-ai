74bd12341b4c3db1de2fba7faea00cd636c67058
Thankgod Morah <thankgod.morah@qucoon.com>
2024-06-28 01:00:12 +0100

    10% mobile

--- src/component/section/PricingSection.tsx
+++ src/component/section/PricingSection.tsx
@@ -6,7 +6,7 @@ export const PricingSection = ()=>{
     return (
         <section className={' py-24'}>
             <div className={'container-responsive relative flex flex-col items-center justify-center'}>
-                    <img className={'w-[400px] h-[400px] absolute -top-40 left-40'} src={imports.image.rocket} alt={''}/>
+                    <img className={'w-[200px] md:w-[400px] h-[200px] md:h-[400px] absolute -top-16 md:-top-40 md:left-40 -left-10'} src={imports.image.rocket} alt={''}/>
                 <h2 className={'text-center font-semibold mb-10'}>
                     <span className={'text-primary-100'}>Go</span> Premium
                 </h2>
