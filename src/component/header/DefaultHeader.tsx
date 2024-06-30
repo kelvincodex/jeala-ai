da50c41bcd25be0b28f17febb662fc9b4ffe99e5
Thankgod Morah <thankgod.morah@qucoon.com>
2024-06-30 18:18:27 +0100

    sidebar

--- src/component/header/DefaultHeader.tsx
+++ src/component/header/DefaultHeader.tsx
@@ -13,7 +13,7 @@ export const DefaultHeader = ()=>{
     const hoverClassName = 'hover:border-b hover:text-primary-100 hover:border-b-primary-100  transition-all duration-300'
 
     function night(){
-
+        console.log('logging')
     }
 
     useEffect(() => {
@@ -30,10 +30,9 @@ export const DefaultHeader = ()=>{
     }, [isClose, staggerMenuItems]);
 
     return (
-        <nav className={'border-b sticky top-0 bg-white z-50 w-full transition-all duration-300 ease-in-out h-[80px] md:h-[100px] '}>
+        <nav className={'shadow sticky top-0 bg-white z-50 w-full transition-all duration-300 ease-in-out h-[80px] md:h-[100px] '}>
             <div className={'flex justify-between h-full items-center container-responsive relative'}>
                 <h2 className={`md:text-[30px] text-[25px]  font-semibold`}><Link to={RoutesConstant.page.home}>Jeala AI</Link></h2>
-
                 {
                     !isClose ?
                         <MenuIcon onClick={()=> setIsClose(!isClose)} className={'w-[30px] h-[30px] cursor-pointer block lg:hidden'}/> :
@@ -49,7 +48,6 @@ export const DefaultHeader = ()=>{
                         <li><MoonIcon onClick={night} className={'w-[40px] h-[40px]'} /></li>
                     </ul>
             </div>
-            {/*${toggleMenu ? 'left-0' : '-left-[100%]'}*/}
             {/*todo mobile*/}
             <ul className={`lg:hidden text-[20px] pt-10 absolute bg-white top-20 gap-5 pl-14 min-w-full ${isClose ? 'left-0' : '-left-[100%]'} transition-left duration-150  h-svh w-full flex flex-col items-left`}>
                 <li className={`${hoverClassName} mobile-lg`}><Link
