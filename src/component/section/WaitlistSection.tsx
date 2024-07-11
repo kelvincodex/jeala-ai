32b26f677774303facdef565dc09f5288b8a77c2
Thankgod Morah <thankgod.morah@qucoon.com>
2024-07-11 20:14:32 +0100

    scroll bar

--- src/component/section/WaitlistSection.tsx
+++ src/component/section/WaitlistSection.tsx
@@ -27,6 +27,7 @@ export const WaitlistSection = ()=>{
 
                 {
                     themeState.theme == 'dark' && (
+
                         <img className={'absolute top-0 w-[1000px] h-[1000px] z-[-9]'} src={importsUtil.image.darkWaitList}
                              alt={''}/>
                     )
