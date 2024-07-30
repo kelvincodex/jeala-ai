45afde85bc28b35bc46591fcf52d452730aeee0d
Thankgod Morah <thankgod.morah@qucoon.com>
2024-07-30 18:37:54 +0100

    footer

--- src/component/footer/DefaultFooter.tsx
+++ src/component/footer/DefaultFooter.tsx
@@ -10,19 +10,19 @@ export const DefaultFooter = () => {
     const hoverClassName = 'hover:border-b hover:text-primary-100 hover:border-b-primary-100  transition-all duration-300'
 
     return(
-        <footer className={'py-12  bg-black-200'}>
+        <footer className={'py-5  bg-black-200'}>
             <motion.div
                 initial={{ opacity: 0, y: -10 }}
                 whileInView={{ opacity: 1, y: 0 }}
                 transition={{ease:'easeOut', duration: 1, delay: 0.2}}
                 className={'container-responsive flex h-full flex-col items-center gap-4'}>
-                <div className={' w-full lg:h-[calc(350px-80px)] h-[calc(350px-100px)] flex lg:flex-row flex-col items-center justify-between'}>
+                <div className={' w-full gap-2  flex lg:flex-row flex-col items-center justify-between'}>
                     <div className={'lg:w-1/2 w-full text-center lg:text-end'}>
                         <h2 className={'md:text-[20px] text-[18px] font-poppins-medium font-bold md:leading-[30px] leading-[20px] text-white mb-4'}>Contact us</h2>
-                        <p className={`font-light md:text-[16px] text-[14px] text-white md:leading-[25px] cursor-pointer  leading-[20px]  ${hoverClassName}`}>info@gramenpetroserve.com;,</p>
-                        <p className={`font-light md:text-[16px] text-[14px] text-white md:leading-[25px] cursor-pointer  leading-[20px]`}>Plot 18 Chevron Clinic Road, Off NPA DSC-Expressway,</p>
-                        <p className={`font-light md:text-[16px] text-[14px]  text-white md:leading-[25px] cursor-pointer  leading-[20px]  `}> Edjeba, Warri, Delta State.</p>
-                        <p className={`font-light md:text-[16px] text-[14px]  text-white md:leading-[25px] cursor-pointer  leading-[20px]  `}>
+                        <p className={`font-[200] md:text-[16px] text-[14px] text-white md:leading-[25px] cursor-pointer  leading-[20px] inline-block  ${hoverClassName}`}>info@gramenpetroserve.com;,</p>
+                        <p className={`font-[200] md:text-[16px] text-[14px] text-white md:leading-[25px] cursor-pointer  leading-[20px]`}>Plot 18 Chevron Clinic Road, Off NPA DSC-Expressway,</p>
+                        <p className={`font-[200] md:text-[16px] text-[14px]  text-white md:leading-[25px] cursor-pointer  leading-[20px]  `}> Edjeba, Warri, Delta State.</p>
+                        <p className={`font-[200] md:text-[16px] text-[14px]  text-white md:leading-[25px] cursor-pointer  leading-[20px]  `}>
                             <a className={`${hoverClassName}`}>+234-803-407-0217</a>
                             ,  <a className={`${hoverClassName}`}>+234-903-855-1651</a>,
                         </p>
@@ -34,7 +34,7 @@ export const DefaultFooter = () => {
                         <FeedbackInput buttonClassName={' md:btn-md btn-sm text-[13px] '} containerClassName={'w-full h-[70px]'} />
                     </div>
                 </div>
-                <div className={'border-t border-white w-full flex flex-col lg:flex-row  items-center justify-between h-[100px] lg:h-[89px] py-5'}>
+                <div className={'border-t border-white w-full gap-2 flex flex-col lg:flex-row  items-center justify-between  pt-3'}>
                     <div className={'flex items-center  gap-2'}>
                         <RoundedTwitter />
                         <RoundedEmail />
@@ -42,9 +42,9 @@ export const DefaultFooter = () => {
                         <RoundedInstagram />
                         <RoundedLinkin />
                     </div>
-                    <div className={'flex flex-col items-end'}>
-                        <p className={'text-white '}>Copyright © 2024 •Jeala AI</p>
-                        <p className={'text-white'}>Privacy & Legal terms</p>
+                    <div className={'flex flex-col items-end '}>
+                        <p className={'text-white text-sm'}>Copyright © 2024 •Jeala AI</p>
+                        <p className={'text-white text-sm'}>Privacy & Legal terms</p>
                     </div>
                 </div>
             </motion.div>
--- src/component/input/FeedbackInput.tsx
+++ src/component/input/FeedbackInput.tsx
@@ -1,10 +1,8 @@
-import {motion} from "framer-motion";
-import {FramerConfigUtil} from "@/util/FramerConfigUtil.ts";
-import {BaseInput} from "@/component/input/BaseInput.tsx";
 import EmailIcon from "@/assets/icon/light-email.svg";
-import {DefaultButton} from "@/component/button/DefaultButton.tsx";
 import {useSelector} from "react-redux";
 import {RootState} from "@/store";
+import { motion } from "framer-motion";
+import {FramerConfigUtil} from "@/util/FramerConfigUtil.ts";
 
 interface FeedbackInputProps {
     containerClassName?: string;
@@ -15,16 +13,28 @@ export const FeedbackInput = ({containerClassName, buttonClassName, inputClassNa
     const themeState = useSelector((state: RootState) => state.theme);
 
     return (
+        // <motion.div
+        //     variants={FramerConfigUtil.fadeUp}
+        //     initial={'hidden'}
+        //     whileInView={'show'}
+        //     transition={{ease: 'easeOut', duration: 1, delay: 0.2}}
+        //     className={`w-full z-[999] flex justify-between items-center  ${themeState.theme == 'light' ? 'bg-white' : 'bg-transparent'} ${containerClassName}  px-1 md:px-3 overflow-hidden rounded-[20px] border gap-3`}>
+        //     <BaseInput leftIcon={EmailIcon} className={`h-full text-[20px] bg-transparent placeholder:text-primary-100 ${inputClassName}`}
+        //                placeholder={'Email Address'}/>
+        //     {/*<Button className={'md:!py-5 !py-2 md:!px-10 !px-5  rounded !text-[10px] md:!text-[18px]'} variant={'contained'}></Button>*/}
+        //     <DefaultButton className={` ${buttonClassName}`}>Join waitlist</DefaultButton>
+        // </motion.div>
+
         <motion.div
             variants={FramerConfigUtil.fadeUp}
             initial={'hidden'}
             whileInView={'show'}
             transition={{ease: 'easeOut', duration: 1, delay: 0.2}}
-            className={`w-full z-[999] flex justify-between items-center  ${themeState.theme == 'light' ? 'bg-white' : 'bg-transparent'} ${containerClassName}  px-1 md:px-3 overflow-hidden rounded-[20px] border gap-3`}>
-            <BaseInput leftIcon={EmailIcon} className={`h-full text-[20px] bg-transparent placeholder:text-primary-100 ${inputClassName}`}
-                       placeholder={'Email Address'}/>
-            {/*<Button className={'md:!py-5 !py-2 md:!px-10 !px-5  rounded !text-[10px] md:!text-[18px]'} variant={'contained'}></Button>*/}
-            <DefaultButton className={` ${buttonClassName}`}>Join waitlist</DefaultButton>
+
+            className={`w-full border h-[60px] ${themeState.theme == 'light' ? 'bg-white' : 'bg-transparent'} rounded-[20px] overflow-hidden  flex items-center px-2 ${containerClassName}`}>
+            <EmailIcon className={'w-[50px]'} />
+            <input placeholder={'Email Address'} className={`outline0-0 focus:outline-none h-full grow bg-transparent px-2 ${inputClassName}`} />
+            <button className={`btn bg-primary-100 border-0 text-white ${buttonClassName}`}>Join Waitlist</button>
         </motion.div>
     )
 }
